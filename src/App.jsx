1dd17d87f86ada9e3aaf2a62ec138681422c0fc0
ngan <vutran@sheridancollege.ca>
2024-05-16 09:40:31 -0400

    Animation using Framer Motion and Layout are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { motion } from "framer-motion"
+
 function App() {
-  
+  return (
+    <div className="bg-zinc-900 min-h-screen grid place-content-center">
+      <div className="grid grid-cols-4 gap-4 w-96 h-96">
+        <div className="bg-zinc-700 col-span-2 row-span-2 rounded-lg"></div>
+        <motion.div whileHover={{scale: 1.1, rotate: 5}} className="bg-zinc-700 rounded-lg"></motion.div>
+        <motion.div whileHover={{scale: 1.1, rotate: 5}} className="bg-zinc-700 rounded-lg"></motion.div>
+        <motion.div whileHover={{scale: 1.1, rotate: 5}} className="bg-zinc-700 rounded-lg"></motion.div>
+        <motion.div whileHover={{scale: 1.1, rotate: 5}} className="bg-zinc-700 rounded-lg"></motion.div>
+        <div className="bg-zinc-700 col-span-full rounded-lg"></div>
+        <div className="bg-zinc-700"></div>
+        <div className="bg-zinc-700 col-span-3 rounded-lg"></div>
+      </div>
+    </div>
+  )
 }
 
 export default App
